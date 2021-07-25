a7888af3e8bdd31ac02f84755105ec198fc47d36
Callum Wright <wrightci01w@gmail.com>
2021-07-25 01:59:24 +0100

    [PW-17] Add profile to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import './Colors.scss';
 import './Home.scss';
 
 import PersonalWebsiteLogo from '../assets/personal-website.png';
+import Profile from '../assets/profile.jpg';
 import SophosLogo from '../assets/sophos.png';
 import WhanalysisLogo from '../assets/whanalysis.png';
 
@@ -13,14 +14,19 @@ export class Home extends Component {
         return (
             <div>
                 <div className='Preface'>
-                    {/* Callum Wright currently works as a Software Engineer III at Sophos, Abingdon. */}
-                    <span className='animated fadeIn' style={{ color: 'var(--color-accent)' }}>Callum Wright</span>
-                    <span className='animated fadeIn animatedDelay500ms'> currently works as a </span>
-                    <span className='animated fadeIn noWrap' style={{ color: 'var(--color-accent)' }}>Software </span>
-                    <span className='animated fadeIn noWrap' style={{ color: 'var(--color-accent)', 'white-space': 'nowrap' }}>Engineer III</span>
-                    <span className='animated fadeIn animatedDelay500ms'> at </span>
-                    <span className='animated fadeIn' style={{ color: 'var(--color-accent)' }}>Sophos</span>
-                    <span className='animated fadeIn animatedDelay500ms'>, Abingdon.</span>
+                    <div class="PrefaceProfile">
+                        <img src={Profile}></img>
+                    </div>
+                    <div>
+                        {/* Callum Wright currently works as a Software Engineer III at Sophos, Abingdon. */}
+                        <span className='animated fadeIn' style={{ color: 'var(--color-accent)' }}>Callum Wright</span>
+                        <span className='animated fadeIn animatedDelay500ms'> currently works as a </span>
+                        <span className='animated fadeIn noWrap' style={{ color: 'var(--color-accent)' }}>Software </span>
+                        <span className='animated fadeIn noWrap' style={{ color: 'var(--color-accent)', 'white-space': 'nowrap' }}>Engineer III</span>
+                        <span className='animated fadeIn animatedDelay500ms'> at </span>
+                        <span className='animated fadeIn' style={{ color: 'var(--color-accent)' }}>Sophos</span>
+                        <span className='animated fadeIn animatedDelay500ms'>, Abingdon.</span>
+                    </div>
                 </div>
                 <div className="Experience">
                     <div class="Professional">
