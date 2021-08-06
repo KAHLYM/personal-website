29be85328fe1767df881c1678bada7eee0620177
Callum Wright <wrightci01w@gmail.com>
2021-08-06 22:14:49 +0100

    [PW-2] Add Experience and ExperienceSection component(s)

---
+++ src/components/ExperienceSection.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import './Animation.scss';
+import './ExperienceSection.scss';
+
+export class ExperienceSection extends Component {
+
+    render() {
+        return (
+            <div className='ExperienceSection'>
+                <div className='ExperienceSectionTitle'>{this.props.title}</div>
+                <div className='ExperienceSectionBody'>{this.props.children}</div>
+            </div>
+        )
+    }
+}
+
+export default ExperienceSection
