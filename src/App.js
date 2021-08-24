8bc37080e45c6762c2e24a5ea92b86b72df6c20b
Callum Wright <wrightci01w@gmail.com>
2021-08-24 19:37:40 +0100

    [PW-31] eslint src/** --fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.scss';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import About from './components/About.js'
-import Footer from './components/Footer.js'
-import Header from './components/Header.js'
-import Home from './components/Home.js'
+import About from './components/About.js';
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
+import Home from './components/Home.js';
 import Portfolio from './components/Portfolio.js';
 import ScrollToTop from './components/ScrollToTop.js';
 import Sophos from './components/Sophos.js';
@@ -16,17 +16,17 @@ function App() {
     <div className="App">
       <Router>
         <ScrollToTop />
-        <Header class="Header"></Header>
-        <div class="Container">
+        <Header class="Header" />
+        <div className="Container">
           <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/sophos' component={Sophos} />
-            <Route path='/portfolio' component={Portfolio} />
-            <Route path='/whanalysis' component={Whanalysis} />
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/sophos" component={Sophos} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/whanalysis" component={Whanalysis} />
           </Switch>
         </div>
-        <Footer class="Footer"></Footer>
+        <Footer class="Footer" />
       </Router>
     </div>
   );
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 import './Social.scss';
 
 import { Github, Gmail, Linkedin } from '@icons-pack/react-simple-icons';
 
 export class Social extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      socialIconGithub: 'rgba(31, 31, 31, 0.64)',
+      socialIconLinkedin: 'rgba(31, 31, 31, 0.64)',
+      socialIconGmailColor: 'rgba(31, 31, 31, 0.64)',
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            socialIconGithub: "rgba(31, 31, 31, 0.64)",
-            socialIconLinkedin: "rgba(31, 31, 31, 0.64)",
-            socialIconGmailColor: "rgba(31, 31, 31, 0.64)",
-        };
-    }
-    
-    render() {
-        return (
-            <div className='SocialWrapper' style={{marginBottom: this.props.bottom + 'px'}}>
-                <a className='SocialIcon animated fadeIn animatedDelay500ms' href={'https://www.github.com/KAHLYM'} target='_blank' rel='noopener noreferrer'>
-                    <Github color={this.state.socialIconGithub} size={this.props.size} onMouseEnter={() => this.setState({ socialIconGithub: '#181717' })} onMouseLeave={() => this.setState({ socialIconGithub: "rgba(31, 31, 31, 0.64)" })} />
-                </a>
-                <a className='SocialIcon animated fadeIn animatedDelay500ms' href={'https://www.linkedin.com/in/callum-wright-81baa3127/'} target='_blank' rel='noopener noreferrer'>
-                    <Linkedin color={this.state.socialIconLinkedin} size={this.props.size} onMouseEnter={() => this.setState({ socialIconLinkedin: '#0077B5' })} onMouseLeave={() => this.setState({ socialIconLinkedin: "rgba(31, 31, 31, 0.64)" })} />
-                </a>
-                <a className='SocialIcon animated fadeIn animatedDelay500ms' href={'mailto:wrightci01w@gmail.com'} style={{marginRight: '0px'}}>
-                    <Gmail color={this.state.socialIconGmailColor} size={this.props.size} onMouseEnter={() => this.setState({ socialIconGmailColor: '#D14836' })} onMouseLeave={() => this.setState({ socialIconGmailColor: "rgba(31, 31, 31, 0.64)" })} />
-                </a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="SocialWrapper" style={{ marginBottom: `${this.props.bottom}px` }}>
+        <a className="SocialIcon animated fadeIn animatedDelay500ms" href="https://www.github.com/KAHLYM" target="_blank" rel="noopener noreferrer">
+          <Github color={this.state.socialIconGithub} size={this.props.size} onMouseEnter={() => this.setState({ socialIconGithub: '#181717' })} onMouseLeave={() => this.setState({ socialIconGithub: 'rgba(31, 31, 31, 0.64)' })} />
+        </a>
+        <a className="SocialIcon animated fadeIn animatedDelay500ms" href="https://www.linkedin.com/in/callum-wright-81baa3127/" target="_blank" rel="noopener noreferrer">
+          <Linkedin color={this.state.socialIconLinkedin} size={this.props.size} onMouseEnter={() => this.setState({ socialIconLinkedin: '#0077B5' })} onMouseLeave={() => this.setState({ socialIconLinkedin: 'rgba(31, 31, 31, 0.64)' })} />
+        </a>
+        <a className="SocialIcon animated fadeIn animatedDelay500ms" href="mailto:wrightci01w@gmail.com" style={{ marginRight: '0px' }}>
+          <Gmail color={this.state.socialIconGmailColor} size={this.props.size} onMouseEnter={() => this.setState({ socialIconGmailColor: '#D14836' })} onMouseLeave={() => this.setState({ socialIconGmailColor: 'rgba(31, 31, 31, 0.64)' })} />
+        </a>
+      </div>
+    );
+  }
 }
 
-export default Social
+export default Social;
