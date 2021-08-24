9f1e31d8db8d51f930d4aa81a1c317d0e5f4829f
Callum Wright <wrightci01w@gmail.com>
2021-08-24 20:07:16 +0100

    Revert "[PW-31] eslint src/** --fix"
    This reverts commit 8bc37080e45c6762c2e24a5ea92b86b72df6c20b.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import Social from './Social';
+import React, { Component } from 'react'
+import Social from './Social'
 
-import './Animation.scss';
+import './Animation.scss'
 import './Colors.scss';
 import './Home.scss';
 
@@ -11,103 +11,104 @@ import SophosLogo from '../assets/sophos.png';
 import WhanalysisLogo from '../assets/whanalysis.png';
 
 export class Home extends Component {
-  componentDidMount() {
-    document.title = 'Callum Wright';
-  }
 
-  nextPath(path) {
-    this.props.history.push(path);
-  }
+    componentDidMount(){
+        document.title = "Callum Wright"
+    }
 
-  render() {
-    return (
-      <div>
-        <div className="Preface">
-          <div className="PrefaceProfile">
-            <img src={Profile} alt="Profile" />
-          </div>
-          <div>
-            <div className="PrefaceText">
-              {/* Callum Wright currently works as a Software Engineer III at Sophos, Abingdon. */}
-              <span className="animated fadeIn" style={{ color: 'var(--color-accent)' }}>Callum Wright</span>
-              <span className="animated fadeIn animatedDelay500ms"> currently works as a </span>
-              <span className="animated fadeIn noWrap" style={{ color: 'var(--color-accent)' }}>Software </span>
-              <span className="animated fadeIn noWrap" style={{ color: 'var(--color-accent)', 'white-space': 'nowrap' }}>Engineer III</span>
-              <span className="animated fadeIn animatedDelay500ms"> at </span>
-              <a className="animated fadeIn Highlight" style={{ color: 'var(--color-accent)' }} href="https://www.sophos.com/en-us.aspx" target="_blank" rel="noopener noreferrer">Sophos</a>
-              <span className="animated fadeIn animatedDelay500ms">, Abingdon.</span>
-            </div>
+    nextPath(path) {
+        this.props.history.push(path);
+    }
+
+    render() {
+        return (
             <div>
-              <Social size="2rem" />
+                <div className='Preface'>
+                    <div class="PrefaceProfile">
+                        <img src={Profile} alt="Profile"></img>
+                    </div>
+                    <div>
+                        <div class="PrefaceText">
+                            {/* Callum Wright currently works as a Software Engineer III at Sophos, Abingdon. */}
+                            <span className='animated fadeIn' style={{ color: 'var(--color-accent)' }}>Callum Wright</span>
+                            <span className='animated fadeIn animatedDelay500ms'> currently works as a </span>
+                            <span className='animated fadeIn noWrap' style={{ color: 'var(--color-accent)' }}>Software </span>
+                            <span className='animated fadeIn noWrap' style={{ color: 'var(--color-accent)', 'white-space': 'nowrap' }}>Engineer III</span>
+                            <span className='animated fadeIn animatedDelay500ms'> at </span>
+                            <a className='animated fadeIn Highlight' style={{ color: 'var(--color-accent)' }} href="https://www.sophos.com/en-us.aspx" target="_blank" rel='noopener noreferrer'>Sophos</a>
+                            <span className='animated fadeIn animatedDelay500ms'>, Abingdon.</span>
+                        </div>
+                        <div>
+                            <Social size='2rem'></Social>
+                        </div>
+                    </div>
+                </div>
+                <div class="ExperienceContainer">
+                    <div class="Professional">
+                        <p>Professional</p>
+                        <div style={{borderRight: "8px solid #225a99"}}>
+                            <div class="Title">
+                                <div>
+                                    <img src={SophosLogo} alt="Sophos Logo"></img>
+                                </div>
+                                <div>
+                                    <p>Sophos</p>
+                                    <p>cybersecurity</p>
+                                    <p>3 yrs 3 mos</p>
+                                    <p onClick={() => this.nextPath('sophos')}>Read More</p>
+                                </div>
+                            </div>
+                            <div class="Details">
+                                <p>Software Engineer III</p>
+                                <p>Jul 2021 - Present · 1 mos</p>
+                                <p>Abingdon, United Kingdon</p>
+                            </div>
+                            <div class="Details">
+                                <p>Software Engineer II</p>
+                                <p>Aug 2019 - Jul 2021 · 2 yrs</p>
+                                <p>Abingdon, United Kingdon</p>
+                            </div>
+                            <div class="Details">
+                                <p>Software Engineer Intern</p>
+                                <p>Jun 2017 - Aug 2018 · 1 yr 3 mos</p>
+                                <p>Abingdon, United Kingdon</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="Personal">
+                        <p>Personal</p>
+                        <div style={{borderRight: "8px solid #4285f4"}}>
+                            <div class="Title">
+                                <div>
+                                    <img src={PersonalWebsiteLogo} alt="Personal Website Logo"></img>
+                                </div>
+                                <div>
+                                    <p>Personal Website</p>
+                                    <p>this website</p>
+                                    <p>Jun 2020 - Jul 2020</p>
+                                    <p onClick={() => this.nextPath('portfolio')}>Read More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{borderRight: "8px solid #128c7e"}}>
+                            <div class="Title">
+                                <div>
+                                    <img src={WhanalysisLogo} alt="Whanlysis Logo"></img>
+                                </div>
+                                <div>
+                                    <p>Whanalysis</p>
+                                    <p>whatsapp analysis</p>
+                                    <p>Dec 2019 - May 2020</p>
+                                    <p onClick={() => this.nextPath('whanalysis')}>Read More</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div className="ExperienceContainer">
-          <div className="Professional">
-            <p>Professional</p>
-            <div style={{ borderRight: '8px solid #225a99' }}>
-              <div className="Title">
-                <div>
-                  <img src={SophosLogo} alt="Sophos Logo" />
-                </div>
-                <div>
-                  <p>Sophos</p>
-                  <p>cybersecurity</p>
-                  <p>3 yrs 3 mos</p>
-                  <p onClick={() => this.nextPath('sophos')}>Read More</p>
-                </div>
-              </div>
-              <div className="Details">
-                <p>Software Engineer III</p>
-                <p>Jul 2021 - Present · 1 mos</p>
-                <p>Abingdon, United Kingdon</p>
-              </div>
-              <div className="Details">
-                <p>Software Engineer II</p>
-                <p>Aug 2019 - Jul 2021 · 2 yrs</p>
-                <p>Abingdon, United Kingdon</p>
-              </div>
-              <div className="Details">
-                <p>Software Engineer Intern</p>
-                <p>Jun 2017 - Aug 2018 · 1 yr 3 mos</p>
-                <p>Abingdon, United Kingdon</p>
-              </div>
-            </div>
-          </div>
-          <div className="Personal">
-            <p>Personal</p>
-            <div style={{ borderRight: '8px solid #4285f4' }}>
-              <div className="Title">
-                <div>
-                  <img src={PersonalWebsiteLogo} alt="Personal Website Logo" />
-                </div>
-                <div>
-                  <p>Personal Website</p>
-                  <p>this website</p>
-                  <p>Jun 2020 - Jul 2020</p>
-                  <p onClick={() => this.nextPath('portfolio')}>Read More</p>
-                </div>
-              </div>
-            </div>
-            <div style={{ borderRight: '8px solid #128c7e' }}>
-              <div className="Title">
-                <div>
-                  <img src={WhanalysisLogo} alt="Whanlysis Logo" />
-                </div>
-                <div>
-                  <p>Whanalysis</p>
-                  <p>whatsapp analysis</p>
-                  <p>Dec 2019 - May 2020</p>
-                  <p onClick={() => this.nextPath('whanalysis')}>Read More</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    );
-  }
+            
+        )
+    }
 }
 
-export default Home;
+export default Home
