6487c7fcf75ae79553c61ed841243f62f5a56f62
Callum Wright <wrightci01w@gmail.com>
2021-08-29 16:57:04 +0100

    [PW-51] Fix text spacing

--- src/components/Sophos.jsx
+++ src/components/Sophos.jsx
@@ -27,25 +27,21 @@ export class Sophos extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Sophos Group plc
+              Sophos Group plc&nbsp;
             </a>
-            {' '}
-            is a British
-            <span className="Highlight">security software</span>
-            {' '}
-            and hardware company that develops network, end user and server protection. More than 200 million
-            users in 150 countries rely on Sophos to provide
-            <span className="Highlight">end-to-end protection</span>
-            {' '}
+            is a British&nbsp;
+            <span className="Highlight">security software</span>&nbsp;
+            and hardware company that develops network, end user and server protection. More than 200 million&nbsp;
+            users in 150 countries rely on Sophos to&nbsp;
+            <span className="Highlight">end-to-end protection</span>&nbsp;
             against threats and data loss.
           </div>
 
           <ExperienceSection title="Role">
             <p className="ExperienceSectionSubTitle">Internship</p>
-            My career at Sophos began as a Software Engineer Intern during my university year in industry. I worked
-            alongside other software engineers of varying levels from graduate to senior. The team that I was
-            assigned to -
-            {' '}
+            My career at Sophos began as a Software Engineer Intern during my university year in industry. I&nbsp;
+            worked alongside other software engineers of varying levels from graduate to senior. The team that I&nbsp;
+            was assigned to -&nbsp;
             <a
               className="Highlight"
               href="https://www.sophos.com/en-us/products/server-security.aspx"
@@ -54,13 +50,12 @@ export class Sophos extends Component {
             >
               Windows Cloud Server
             </a>
-            , developed three products: Update Cache, Message Relay and Server Lockdown.
+            , developed three products: Update Cache, Message Relay and Server Lockdown.&nbsp;
             <br />
             <br />
             <p className="ExperienceSectionSubTitle">Post Graduation</p>
-            Subsequently, after graduating I returned to Sophos as a Software Engineer II and to a different team,
-            I was introduced to
-            {' '}
+            Subsequently, after graduating I returned to Sophos as a Software Engineer II and to a different team,&nbsp;
+            I was introduced to&nbsp;
             <a
               className="Highlight"
               href="https://www.hitmanpro.com/en-us/alert"
@@ -69,15 +64,14 @@ export class Sophos extends Component {
             >
               HitmanPro.Alert
             </a>
-            ; a behavioural based exploit mitigation product. Being behavioural based, the product was susceptible
-            to false positives. My initial focus was to accommodate existing customer requirements and develop features
-            to
-            <span className="Highlight">support and streamline implementation</span>
-            {' '}
+            ; a behavioural based exploit mitigation product. Being behavioural based, the product was susceptible&nbsp;
+            to false positives. My initial focus was to accommodate existing customer requirements and develop&nbsp;
+            features to&nbsp;
+            <span className="Highlight">support and streamline implementation</span>&nbsp;
             of similar requirements in future.
             <p className="ExperienceSectionSubTitle">Further Experience</p>
-            As my understanding of the product increased I developed broader aspects of work i.e refactoring the
-            tracking of system proceses and associated metadata to reduce complexity and increase maintainability
+            As my understanding of the product increased I developed broader aspects of work i.e refactoring the&nbsp;
+            tracking of system proceses and associated metadata to reduce complexity and increase maintainability&nbsp;
             while eliminating various edge cases.
           </ExperienceSection>
 
@@ -85,25 +79,25 @@ export class Sophos extends Component {
 
           <ExperienceSection title="Learnings">
             <p className="ExperienceSectionSubTitle">Internship</p>
-            The placement provided a valuable introduction to the software industry, allowing me to gain a clear
-            understanding of industry practices i.e. Agile Methodology and appropriate software and processes.
-            Writing predominantly in C++ allowed me to consolidate my understanding of object orientated programming
-            concepts which can be translated across many languages.
+            The placement provided a valuable introduction to the software industry, allowing me to gain a clear&nbsp;
+            understanding of industry practices i.e. Agile Methodology and appropriate software and processes.&nbsp;
+            Writing predominantly in C++ allowed me to consolidate my understanding of object orientated&nbsp;
+            programming concepts which can be translated across many languages.
             <br />
             <br />
             <p className="ExperienceSectionSubTitle">Post Graduation and Further Experience</p>
-            Working on
-            {' '}
+            Working on&nbsp;
             <a
               className="Highlight"
               href="https://www.hitmanpro.com/en-us/alert"
               target="_blank"
               rel="noopener noreferrer"
             >
-              HitmanPro.Alert
+              HitmanPro.Alert&nbsp;
             </a>
-            , low level driver features highlighted the importance of writing high quality code with thorough error
-            handling to accommodate edge cases that could result in critical failure. This further enforced the
+            , low level driver features highlighted the importance of writing high quality code with thorough&nbsp;
+            error handling to accommodate edge cases that could result in critical failure. This further enforced&nbsp;
+            the&nbsp;
             <span className="Highlight">importance of testing</span>
             .
           </ExperienceSection>
--- src/components/Whanalysis.jsx
+++ src/components/Whanalysis.jsx
@@ -27,16 +27,15 @@ export class Whanalysis extends Component {
           </div>
 
           <ExperienceSection title="Role">
-            Working solely on Whanalysis, I managed
-            {' '}
+            Working solely on Whanalysis, I managed&nbsp;
             <span className="Highlight">all aspects from conception to release</span>
             ; be that but not limited to design, programming and SEO optimization.
           </ExperienceSection>
 
           <ExperienceSection title="Motivation">
-            The concept was conceived long ago as a means to learn Python. Developed without consideration of the
-            analysis to be accessed remotely, conversing with friends and colleagues highlighted the difficulties
-            demonstrating the process and displaying the analysis. These difficulties later presented a great
+            The concept was conceived long ago as a means to learn Python. Developed without consideration of the&nbsp;
+            analysis to be accessed remotely, conversing with friends and colleagues highlighted the difficulties&nbsp;
+            demonstrating the process and displaying the analysis. These difficulties later presented a great&nbsp;
             opportunity when motivated to learn a new web development framework.
           </ExperienceSection>
 
@@ -44,8 +43,7 @@ export class Whanalysis extends Component {
 
           <ExperienceSection title="Process">
             <p className="ExperienceSectionSubTitle">Material Design</p>
-            Given that Angular was the chosen framework, Material Design was the obvious choice.
-            {' '}
+            Given that Angular was the chosen framework, Material Design was the obvious choice.&nbsp;
             <a
               className="Highlight"
               href="https://material.angular.io/"
@@ -54,22 +52,20 @@ export class Whanalysis extends Component {
             >
               Angular Material
             </a>
-            {' '}
-            offered intuitive and versatile components, optimized for web, mobile and desktop that was backed by
-            research to establish thorough design principles. These features enabled me to focus on learning Angular
-            and
+            &nbsp;offered intuitive and versatile components, optimized for web, mobile and desktop that was backed&nbsp;
+            by research to establish thorough design principles. These features enabled me to focus on learning&nbsp;
+            Angular and&nbsp;
             <span className="Highlight">increase development velocity</span>
             .
             <p className="ExperienceSectionSubTitle">Internationalization</p>
-            It became apparent that WhatsApp does not standardize their datetime format. Although there exist functions
-            that will attempt to parse a string without a specified format, they do not guarantee that the correct
-            format will be found. To mitigate any issue, the user is asked to enter the datetime used in their chat
-            with aid from an example from their chat and validation.
+            It became apparent that WhatsApp does not standardize their datetime format. Although there exist&nbsp;
+            functions that will attempt to parse a string without a specified format, they do not guarantee that&nbsp;
+            the correct format will be found. To mitigate any issue, the user is asked to enter the datetime used&nbsp;
+            in their chat with aid from an example from their chat and validation.
             <p className="ExperienceSectionSubTitle">Security</p>
-            The ability to access analysis was one of the driving factors that motivated this project. With an
-            increase in the ability to access data there is often an increase in security risk and it is important
-            to obtain a
-            {' '}
+            The ability to access analysis was one of the driving factors that motivated this project. With an&nbsp;
+            increase in the ability to access data there is often an increase in security risk and it is important&nbsp;
+            to obtain a&nbsp;
             <span className="Highlight">balance between accessibility and privacy</span>
             . The ability to share analysis emphasised the importance of these concerns.
             <br />
@@ -82,31 +78,29 @@ export class Whanalysis extends Component {
             >
               Google Firebase
             </a>
-            {' '}
-            recommended security rules to block unauthorized access to data; these were enforced. In
+            &nbsp;recommended security rules to block unauthorized access to data; these were enforced. In&nbsp;
             <span className="Highlight">accordance with GDPR</span>
-            , data is retained for the limited time that is deemed necessary. The user submitted chat messages are
-            deleted once processed and the resulting analysis is stored for 72 hours to enable the ability to share
-            the analysis. Having cleanup scripts scheduled to run through automated cloud functions helped Whanalysis
-            to remain in accordance to privacy law and reduce maintenance.
+            , data is retained for the limited time that is deemed necessary. The user submitted chat messages are&nbsp;
+            deleted once processed and the resulting analysis is stored for 72 hours to enable the ability to&nbsp;
+            share the analysis. Having cleanup scripts scheduled to run through automated cloud functions helped&nbsp;
+            Whanalysis to remain in accordance to privacy law and reduce maintenance.
             <br />
             <br />
-            The user is given an access code which they may share with others. This is a justified tradeoff between
-            shareability and security. The user is reminded of the security concerns.
+            The user is given an access code which they may share with others. This is a justified tradeoff&nbsp;
+            between shareability and security. The user is reminded of the security concerns.
             <p className="ExperienceSectionSubTitle">Demo</p>
-            People are inpatient; they want to see results fast. With a single click, the user has insight into what
-            is available. A demo should ease privacy concerns as the user need not sacrifice any of their privacy to
-            understand what is available.
+            People are inpatient; they want to see results fast. With a single click, the user has insight into&nbsp;
+            what is available. A demo should ease privacy concerns as the user need not sacrifice any of their&nbsp;
+            privacy to understand what is available.
           </ExperienceSection>
 
           <img className="ExperienceSectionBody" src={HumaaansWhanalysisProcess} alt="Whanalysis Process" />
 
           <ExperienceSection title="Review">
             Angular provided a
-            {' '}
             <span className="Highlight">holistic experience</span>
-            ; one that was less challenging than anticipated given the higher complexity in comparison to alternatives
-            React and
+            ; one that was less challenging than anticipated given the higher complexity in comparison to&nbsp;
+            alternatives React and&nbsp;
             <a
               className="Highlight"
               href="https://vuejs.org/"
@@ -115,14 +109,14 @@ export class Whanalysis extends Component {
             >
               Vue
             </a>
-            . Being a fully-fledged framework, Angular eliminated the choice of libraries and allowed focus to remain
-            on the concepts rather than managerial decisions. With previous experience in type-based programming, the
-            mandate to use TypeScript was welcome and offered a familiar experience. Having chosen another
-            Google-backed technology, Firebase provided frictionless integration with Angular.
+            . Being a fully-fledged framework, Angular eliminated the choice of libraries and allowed focus to&nbsp;
+            remain on the concepts rather than managerial decisions. With previous experience in type-based&nbsp;
+            programming, the mandate to use TypeScript was welcome and offered a familiar experience. Having&nbsp;
+            chosen another Google-backed technology, Firebase provided frictionless integration with Angular.
             <br />
             <br />
-            The importance of testing became apparent and was highlighted by various avoidable and time consuming
-            scenarios. Although the Python scripts were extensively tested, the Angular components were not. Given
+            The importance of testing became apparent and was highlighted by various avoidable and time consuming&nbsp;
+            scenarios. Although the Python scripts were extensively tested, the Angular components were not. Given&nbsp;
             more time to work on the project, test coverage would be the primary focus.
           </ExperienceSection>
 
