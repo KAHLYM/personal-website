7e622ebe597e28359eab776ef50c8ce9c9880009
Personal Website Lint Action <wrightci01s@gmail.com>
2021-09-04 00:25:37 +0000

    Fix code style issues with ESLint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,22 +6,22 @@ import resume from '../assets/Resume.pdf';
 import ThemeToggle from './ThemeToggle';
 
 function Header() {
-  return (
-    <div className="Header animated fadeIn animatedDelay500ms">
-      <div className="HeaderLeft">
-        <ThemeToggle />
-        <a className="HeaderNavLink" href={resume} target="_blank" rel="noopener noreferrer">RÉSUMÉ</a>
-        <NavLink to="/about" activeClassName="IsActive">
-          <span className="HeaderNavLink">ABOUT</span>
-        </NavLink>
-      </div>
-      <div className="HeaderRight">
-        <NavLink to="/" exact activeClassName="IsActive">
-          <span className="HeaderNavLink">HOME</span>
-        </NavLink>
-      </div>
-    </div>
-  );
+    return (
+      <div className="Header animated fadeIn animatedDelay500ms">
+          <div className="HeaderLeft">
+              <ThemeToggle />
+              <a className="HeaderNavLink" href={resume} target="_blank" rel="noopener noreferrer">RÉSUMÉ</a>
+              <NavLink to="/about" activeClassName="IsActive">
+                  <span className="HeaderNavLink">ABOUT</span>
+                </NavLink>
+            </div>
+          <div className="HeaderRight">
+              <NavLink to="/" exact activeClassName="IsActive">
+                  <span className="HeaderNavLink">HOME</span>
+                </NavLink>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,58 +6,58 @@ import { Github, Gmail, Linkedin } from '@icons-pack/react-simple-icons';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Social extends Component {
-  render() {
-    const { bottom, size } = this.props;
-    return (
-      <div className="SocialWrapper" style={{ marginBottom: bottom }}>
-        <a
-          className="SocialIcon animated fadeIn animatedDelay500ms"
-          href="https://www.github.com/KAHLYM"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github
-            color="rgba(31, 31, 31, 0.64)"
-            size={size}
-            id="IconGithub"
-          />
-        </a>
-        <a
-          className="SocialIcon animated fadeIn animatedDelay500ms"
-          href="https://www.linkedin.com/in/callum-wright-81baa3127/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Linkedin
-            color="rgba(31, 31, 31, 0.64)"
-            size={size}
-            id="IconLinkedin"
-          />
-        </a>
-        <a
-          className="SocialIcon animated fadeIn animatedDelay500ms"
-          href="mailto:wrightci01w@gmail.com"
-          style={{ marginRight: '0px' }}
-        >
-          <Gmail
-            color="rgba(31, 31, 31, 0.64)"
-            size={size}
-            id="IconGmail"
-          />
-        </a>
-      </div>
-    );
-  }
+    render() {
+        const { bottom, size } = this.props;
+        return (
+          <div className="SocialWrapper" style={{ marginBottom: bottom }}>
+              <a
+                  className="SocialIcon animated fadeIn animatedDelay500ms"
+                  href="https://www.github.com/KAHLYM"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Github
+                      color="rgba(31, 31, 31, 0.64)"
+                      size={size}
+                      id="IconGithub"
+                    />
+                </a>
+              <a
+                  className="SocialIcon animated fadeIn animatedDelay500ms"
+                  href="https://www.linkedin.com/in/callum-wright-81baa3127/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Linkedin
+                      color="rgba(31, 31, 31, 0.64)"
+                      size={size}
+                      id="IconLinkedin"
+                    />
+                </a>
+              <a
+                  className="SocialIcon animated fadeIn animatedDelay500ms"
+                  href="mailto:wrightci01w@gmail.com"
+                  style={{ marginRight: '0px' }}
+                >
+                  <Gmail
+                      color="rgba(31, 31, 31, 0.64)"
+                      size={size}
+                      id="IconGmail"
+                    />
+                </a>
+            </div>
+        );
+    }
 }
 
 Social.propTypes = {
-  bottom: PropTypes.string,
-  size: PropTypes.string,
+    bottom: PropTypes.string,
+    size: PropTypes.string,
 };
 
 Social.defaultProps = {
-  bottom: '0rem',
-  size: '1rem',
+    bottom: '0rem',
+    size: '1rem',
 };
 
 export default Social;
