c19e0d10c17b1d147678306916b3deb3ec187a8c
Callum Wright <wrightci01w@gmail.com>
2021-09-21 19:36:51 +0100

    [PW-119] Fix (camelcase)

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,17 +1,17 @@
 import { getAnalytics, logEvent } from "firebase/analytics";
 
-export function LogEventExternalLink(item_id) {
+export function LogEventExternalLink(itemId) {
     const analytics = getAnalytics();
     logEvent(analytics, 'select_content', {
         content_type: "external_link",
-        item_id: item_id
+        item_id: itemId
     });
 }
 
-export function LogEventTheme(item_id) {
+export function LogEventTheme(itemId) {
     const analytics = getAnalytics();
     logEvent(analytics, 'select_content', {
         content_type: "theme",
-        item_id: item_id
+        item_id: itemId
     });
 }
